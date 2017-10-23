f523d27c2e7116d819fff6895cea2851ae9b9166
Guilherme Porto <guilherme.porto@lambda3.com.br>
2017-10-22 23:46:20 -0200

    Edicao de cursos

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,20 @@
 import * as types from '../actions/actionTypes'
+import initialState from './initialState'
 
-export default function courseReduce(state = [], action) {
+export default function courseReduce(state = initialState.courses, action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses
+    case types.CREATE_AUTHOR_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.course)
+      ]
+    case types.UPDATE_AUTHOR_SUCCESS:
+      return [
+        ...state.filter(course => course.id != action.course.id),
+        Object.assign({}, action.course)
+      ]
     default:
       return state
   }
