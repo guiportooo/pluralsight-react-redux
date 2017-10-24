1f6014b04d6ce67695e82f4671108e4f0e56286e
Guilherme Porto <guilherme.porto@lambda3.com.br>
2017-10-23 23:53:44 -0200

    Tratando async status e erros

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes'
 import courseApi from '../api/mockCourseApi'
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions'
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses }
@@ -15,6 +16,7 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
   return dispatch => {
+    dispatch(beginAjaxCall())
     return courseApi.getAllCourses()
       .then(courses => {
         dispatch(loadCoursesSuccess(courses))
@@ -27,6 +29,7 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function(dispatch) {
+    dispatch(beginAjaxCall())    
     return courseApi.saveCourse(course)
       .then(savedCourse => {
         course.id ? 
@@ -34,6 +37,7 @@ export function saveCourse(course) {
         dispatch(createCourseSuccess(savedCourse))
     })
     .catch(error => {
+      dispatch(ajaxCallError())        
       throw(error)
     })
   }
